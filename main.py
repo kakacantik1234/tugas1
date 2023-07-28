95cfb99b101b64f63e2dd7241ed1144a705bf91e
英德丽kakacantik1234 <140225155+kakacantik1234@users.noreply.github.com>
2023-07-28 16:30:52 +0700

    tugas1
    learning python sampai chapter 4 per tanggal 28 juli 2023

---
+++ main.py
@@ -0,0 +1,31 @@
+import time
+start_time = time.time()
+print("hello world")
+print("世界")
+print("您好")
+print("世界，您好")
+
+
+print("您们好")
+#这是评论
+a=10
+
+
+print(a)
+for i in range(1,1000):
+    a = 10
+
+print(time.time() - start_time, "detik")
+# kita bisa mengcompile python ke 
+# yang namanya bytecode
+# cara mengcompile, buka terminal dan tuliskan
+# python -m py_compile Main.py
+property
+>>>10
+10
+>>>10+5
+15
+
+
+
+
---
+++ tugas1.py
@@ -0,0 +1,64 @@
+# variabel  adalah tempat menyimpan data 
+
+a = 10 
+x = 5
+panjang = 1000
+
+print("Nilai a = ", a)
+print("Nilai x = ", x)
+print("Nilai panjang = ", panjang)
+
+#penamaan 
+nilai_y = 15  #dengan menggunakan underscore 
+juta10 = 10000000 #ini boleh 
+nilaiZ = 17.5 #ini boleh 
+
+#pemanggilan kedua 
+print("Nilai a = ", a)
+a = 7 
+print("Nilai a = ", a)
+
+#assignment indirect
+b = a 
+print("Nilai b = ", b)
+
+# a = 10, a adalah variabel dengan nilai 10 
+
+# tipe data:Angka satuan (integer)
+data_integer = 11 
+print(type(data_integer))
+print("data: ", data_integer)
+print("- bertipe: ", type(data_integer))
+
+#tipe data: angka dengan koma (float)
+
+data_float = 1.5
+print("data: ", data_float)
+print("- bertipe: ", type(data_float))
+
+#tipe data: kumpulan karakter (string)
+
+data_string = "ucup"
+print("data: ", data_string)
+print("- bertipe: ", type(data_string))
+
+#tipe data: biner true/false (boolean)
+
+data_bool = False 
+print("data: ", data_bool)
+print("- bertipe: ", type(data_bool))
+
+##tipe data khusus
+
+#bilangan kompleks
+data_complex = complex(5,6) 
+print("data: ", data_complex)
+print("- bertipe: ", type(data_complex))
+
+#tipe data dari bahasa C 
+
+from ctypes import c_double
+
+data_c_double = c_double(10.5)
+print("data: ", data_c_double)
+print("- bertipe: ", type(data_c_double))
